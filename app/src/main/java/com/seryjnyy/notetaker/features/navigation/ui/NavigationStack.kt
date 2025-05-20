805fef115b32bcb5505aa00f1eabf17a5ff4a54e
Jakub Wojcik <jakubwojcik6a@gmail.com>
2025-05-20 17:41:02 +0100

    Create navigation stack with initial routes
    - Added notes list and notes detail to navigation stack.

---
+++ app/src/main/java/com/seryjnyy/notetaker/features/navigation/ui/NavigationStack.kt
@@ -0,0 +1,35 @@
+package com.seryjnyy.notetaker.features.navigation.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.seryjnyy.notetaker.features.navigation.NoteDetail
+import com.seryjnyy.notetaker.features.navigation.NoteList
+import com.seryjnyy.notetaker.features.notes.ui.NoteDetailScreen
+import com.seryjnyy.notetaker.features.notes.ui.NotesListScreen
+
+@Composable
+fun NavigationStack(
+) {
+val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = NoteList()
+    ){
+        composable<NoteList> {
+            NotesListScreen(
+                navigateToNewNote = { navController.navigate(NoteDetail(null)) },
+                navigateToNoteDetail = { noteId -> navController.navigate(NoteDetail(noteId)) }
+            )
+    }
+        composable<NoteDetail> {
+            val noteId = it.toRoute<NoteDetail>().noteId
+            NoteDetailScreen(
+                noteId = noteId,
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
