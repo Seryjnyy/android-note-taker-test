8c98cc1e41b264eb2df1f38004885312bd566785
Jakub Wojcik <jakubwojcik6a@gmail.com>
2025-05-20 18:06:33 +0100

    Update note repo to convert Note entity to model

--- app/src/main/java/com/seryjnyy/notetaker/data/repository/NoteRepository.kt
+++ app/src/main/java/com/seryjnyy/notetaker/data/repository/NoteRepository.kt
@@ -1,14 +1,16 @@
 package com.seryjnyy.notetaker.data.repository
 
+import com.seryjnyy.notetaker.data.domain.model.toNote
 import com.seryjnyy.notetaker.data.local.db.dao.NoteDao
 import com.seryjnyy.notetaker.data.local.db.entity.NoteEntity
+import kotlinx.coroutines.flow.map
 
 class NoteRepository(
     private val noteDao: NoteDao
 ) {
-    fun observeAllNotes() = noteDao.observeAllNotes()
+    fun observeAllNotes() = noteDao.observeAllNotes().map{ list -> list.map { noteEntity -> noteEntity.toNote() } }
 
-    fun observeNote(id: Long) = noteDao.observeNote(id)
+    fun observeNote(id: Long) = noteDao.observeNote(id).map { it?.toNote() }
 
     suspend fun upsertNote(note: NoteEntity) = noteDao.upsertNote(note)
 
