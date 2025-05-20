1a18eeceb8fb6679ef798c67cede6b8fc0924fa5
Jakub Wojcik <jakubwojcik6a@gmail.com>
2025-05-20 18:03:42 +0100

    Add AndroidEntryPoint to main activity for DI

--- app/src/main/java/com/seryjnyy/notetaker/MainActivity.kt
+++ app/src/main/java/com/seryjnyy/notetaker/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.seryjnyy.notetaker.features.navigation.ui.NavigationStack
 import com.seryjnyy.notetaker.ui.theme.NoteTakerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
