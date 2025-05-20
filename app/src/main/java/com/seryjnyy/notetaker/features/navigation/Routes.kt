9b16a95d118ad92a644a0fba18241af2cf6da431
Jakub Wojcik <jakubwojcik6a@gmail.com>
2025-05-20 17:39:49 +0100

    Add note-list & note-detail routes

---
+++ app/src/main/java/com/seryjnyy/notetaker/features/navigation/Routes.kt
@@ -0,0 +1,9 @@
+package com.seryjnyy.notetaker.features.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class NoteList
+
+@Serializable
+class NoteDetail(val noteId: Long?)
