225c042bd437da1928f2810b4fe2122a58f69812
Jakub Wojcik <jakubwojcik6a@gmail.com>
2025-05-20 16:53:32 +0100

    Add initial db setup
    - Created db.
    
    - Created initial note entity.

---
+++ app/src/main/java/com/seryjnyy/notetaker/data/local/db/AppDatabase.kt
@@ -0,0 +1,14 @@
+package com.seryjnyy.notetaker.data.local.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.seryjnyy.notetaker.data.local.db.entity.NoteEntity
+
+@Database(
+    entities = [
+        NoteEntity::class
+    ], version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+
+}
---
+++ app/src/main/java/com/seryjnyy/notetaker/data/local/db/entity/NoteEntity.kt
@@ -0,0 +1,11 @@
+package com.seryjnyy.notetaker.data.local.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "notes"
+)
+data class NoteEntity(
+    @PrimaryKey val id: Long = 0,
+)
