b086390251ec2e978ddefd0be0d57a7f612cf7a9
Jakub Wojcik <jakubwojcik6a@gmail.com>
2025-05-22 18:24:01 +0100

    Initialise Firebase in MainActivity
    - Needs to be done to make Firebase work.

--- app/src/main/java/com/seryjnyy/notetaker/MainActivity.kt
+++ app/src/main/java/com/seryjnyy/notetaker/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.google.firebase.FirebaseApp
 import com.seryjnyy.notetaker.features.navigation.ui.NavigationStack
 import com.seryjnyy.notetaker.ui.theme.NoteTakerTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +13,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        FirebaseApp.initializeApp(this)
+
         enableEdgeToEdge()
         setContent {
             NoteTakerTheme(
