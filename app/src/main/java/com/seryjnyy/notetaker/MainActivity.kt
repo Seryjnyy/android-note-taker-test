9d4411d2e023794f57bb69ba792de8c4e3df3c4a
Jakub Wojcik <jakubwojcik6a@gmail.com>
2025-05-22 18:19:30 +0100

    Add new theme
    - Added new theme.
    
    - Disabled dynamic colours to see theme.

--- app/src/main/java/com/seryjnyy/notetaker/MainActivity.kt
+++ app/src/main/java/com/seryjnyy/notetaker/MainActivity.kt
@@ -14,7 +14,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NoteTakerTheme {
+            NoteTakerTheme(
+                dynamicColor = false
+            ) {
                 NavigationStack()
             }
         }
