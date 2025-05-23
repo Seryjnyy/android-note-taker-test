a8d382241c1c6b08f35c1bc585ef3954f18d32d8
Jakub Wojcik <jakubwojcik6a@gmail.com>
2025-05-23 01:58:17 +0100

    Update navigation stack to include sync settings

--- app/src/main/java/com/seryjnyy/notetaker/features/navigation/ui/NavigationStack.kt
+++ app/src/main/java/com/seryjnyy/notetaker/features/navigation/ui/NavigationStack.kt
@@ -6,12 +6,17 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
 import androidx.navigation.toRoute
+import com.seryjnyy.notetaker.R
 import com.seryjnyy.notetaker.features.navigation.NoteDetail
 import com.seryjnyy.notetaker.features.navigation.NoteList
 import com.seryjnyy.notetaker.features.navigation.SettingSync
@@ -20,14 +25,19 @@ import com.seryjnyy.notetaker.features.navigation.SettingSyncNav
 import com.seryjnyy.notetaker.features.navigation.Settings
 import com.seryjnyy.notetaker.features.notes.ui.detail.NoteDetailScreen
 import com.seryjnyy.notetaker.features.notes.ui.list.NotesListScreen
+import com.seryjnyy.notetaker.features.auth.AuthState
+import com.seryjnyy.notetaker.features.auth.AuthViewModel
 import com.seryjnyy.notetaker.features.settings.ui.SettingPage
+import com.seryjnyy.notetaker.features.settings.ui.SettingSyncAccountSection
 import com.seryjnyy.notetaker.features.settings.ui.SettingSyncLoginSection
+import com.seryjnyy.notetaker.features.settings.ui.SettingSyncSection
 import com.seryjnyy.notetaker.features.settings.ui.SettingsScreen
 
 @Composable
 fun NavigationStack(
 ) {
 val navController = rememberNavController()
+    val authViewModel: AuthViewModel = viewModel()
     NavHost(
         navController = navController,
         startDestination = NoteList()
@@ -55,7 +65,39 @@ val navController = rememberNavController()
             startDestination = SettingSync()
         ){
             composable<SettingSync> {
-                Text("Setting sync")
+
+                val authState by authViewModel.authState.collectAsStateWithLifecycle()
+
+                val isAuthenticated = authState is AuthState.Authenticated
+                val isSyncOn = true
+
+                SettingPage(
+                    navController = navController,
+                    topBarTitle = "Sync",
+                    topBarIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_sync),
+                            contentDescription = "Sync icon"
+                        )
+                    },
+                    listOfSections = listOf(
+                        {
+                            SettingSyncSection(
+                                isUserAuthenticated = isAuthenticated,
+                                isSyncOn = isSyncOn && isAuthenticated
+                            )
+                            SettingSyncAccountSection(
+                                navigateToLoginScreen = { navController.navigate(SettingSyncLogin) },
+                                user = when(authState){
+                                    is AuthState.Authenticated -> (authState as AuthState.Authenticated).user
+                                    else -> null
+                                },
+                                signOut = { authViewModel.signOut() }
+                            )
+//                        )
+                        }
+                    )
+                )
             }
             composable<SettingSyncLogin> {
                 SettingPage(
