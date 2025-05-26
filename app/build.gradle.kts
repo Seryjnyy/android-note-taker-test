cee8b827b95ae2a718add5bf913483cacc6fa2f2
Jakub Wojcik <jakubwojcik6a@gmail.com>
2025-05-26 21:05:01 +0100

    Add firestore lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,8 @@ dependencies {
 
     // Firebase
     implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
     // optional - needed for credentials support from play services, for devices running
