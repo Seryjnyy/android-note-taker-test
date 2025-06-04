722a6a82605fbe8ff6485fd8fd8828c52cf70fa7
Jakub Wojcik <jakubwojcik6a@gmail.com>
2025-06-04 15:28:01 +0100

    Added DI for firestore and firebase auth

---
+++ app/src/main/java/com/seryjnyy/notetaker/di/FirebaseModule.kt
@@ -0,0 +1,21 @@
+package com.seryjnyy.notetaker.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+    @Provides
+    @Singleton
+    fun provideFirestore() = FirebaseFirestore.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideAuth() = FirebaseAuth.getInstance()
+}
